3e46927a4a4d947cd669998dad7c57978421819a
AndradeLaryssa <76976336+AndradeLaryssa@users.noreply.github.com>
2021-06-30 15:58:41 -0300

    Create Locadora_main.py

---
+++ Locadora_main.py
@@ -0,0 +1,11 @@
+import Locadora_funcoes
+
+listaQuilometros = []
+listaMulheres = []
+
+#cadastrar cliente
+funcoes.cadastrarCliente(listaQuilometros, listaMulheres)
+#calcular média de quilometros
+funcoes.calcularMediakm(listaQuilometros)
+#listar mulheres 
+funcoes.listarMulheres(listaMulheres)
